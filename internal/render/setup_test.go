97d658bf035851c3c8bdbfe887f893ea65e63149
RazikaBengana <razikabengana@gmail.com>
2024-06-10 01:12:20 +0200

    Set up the testing environment with `setup_test.go`

---
+++ internal/render/setup_test.go
@@ -0,0 +1,52 @@
+package render
+
+import (
+	"encoding/gob"
+	"github.com/RazikaBengana/Go-BnB/internal/config"
+	"github.com/RazikaBengana/Go-BnB/internal/models"
+	"github.com/alexedwards/scs/v2"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+
+	// What I am going to put in the session
+	gob.Register(models.Reservation{})
+
+	// Change this to true when in production to enforce secure settings
+	testApp.InProduction = false
+
+	// Initialize the session manager with a 24-hour session lifetime
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	// Store the session manager in the app config
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
+
+type myWriter struct{}
+
+func (tw *myWriter) Header() http.Header {
+	var h http.Header
+	return h
+}
+
+func (tw *myWriter) WriteHeader(i int) {}
+
+func (tw *myWriter) Write(b []byte) (int, error) {
+	length := len(b)
+	return length, nil
+}
