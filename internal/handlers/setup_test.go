092aab4d29844dcead043ffdc0a59ac56062c800
RazikaBengana <razikabengana@gmail.com>
2024-06-09 19:56:17 +0200

    Add `setup_test.go` to initialize routes, session management, middleware, and template caching for testing

---
+++ internal/handlers/setup_test.go
@@ -0,0 +1,136 @@
+package handlers
+
+import (
+	"encoding/gob"
+	"fmt"
+	"github.com/RazikaBengana/Go-BnB/internal/config"
+	"github.com/RazikaBengana/Go-BnB/internal/models"
+	"github.com/RazikaBengana/Go-BnB/internal/render"
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/justinas/nosurf"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"time"
+)
+
+var app config.AppConfig
+var session *scs.SessionManager
+var pathToTemplates = "./../../templates"
+var functions = template.FuncMap{}
+
+func getRoutes() http.Handler {
+	// What I am going to put in the session
+	gob.Register(models.Reservation{})
+
+	// Change this to true when in production to enforce secure settings
+	app.InProduction = false
+
+	// Initialize the session manager with a 24-hour session lifetime
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	// Store the session manager in the app config
+	app.Session = session
+
+	// Create a template cache for rendering HTML templates
+	tc, err := CreateTestTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = true
+
+	repo := NewRepo(&app)
+	NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer) // Add middleware to recover from panics and return a 500 error
+	mux.Use(NoSurf)               // Add CSRF protection middleware to all routes
+	mux.Use(SessionLoad)          // Load and save the session on every request
+
+	mux.Get("/", Repo.Home)
+	mux.Get("/about", Repo.About)
+	mux.Get("/generals-quarters", Repo.Generals)
+	mux.Get("/majors-suite", Repo.Majors)
+
+	mux.Get("/search-availability", Repo.Availability)
+	mux.Post("/search-availability", Repo.PostAvailability)
+	mux.Post("/search-availability-json", Repo.AvailabilityJSON)
+
+	mux.Get("/contact", Repo.Contact)
+
+	mux.Get("/make-reservation", Repo.Reservation)
+	mux.Post("/make-reservation", Repo.PostReservation)
+	mux.Get("/reservation-summary", Repo.ReservationSummary)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
+
+	return mux
+}
+
+// NoSurf adds CSRF protection to all POST requests
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,                 // Ensure the cookie is not accessible via JavaScript
+		Path:     "/",                  // Set the cookie to be accessible site-wide
+		Secure:   app.InProduction,     // Indicate whether the cookie should only be sent over HTTPS
+		SameSite: http.SameSiteLaxMode, // Provide some protection against CSRF attacks
+	})
+	return csrfHandler
+}
+
+// SessionLoad loads and saves the session on every request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
+}
+
+// CreateTestTemplateCache creates a cache for templates
+func CreateTestTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	// Get all the files named *.page.tmpl from ./templates
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
+	if err != nil {
+		return myCache, err
+	}
+
+	// Range through all files ending with *.page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		// Add the template set to the cache
+		myCache[name] = ts
+	}
+
+	// Return the populated template cache
+	return myCache, nil
+}
