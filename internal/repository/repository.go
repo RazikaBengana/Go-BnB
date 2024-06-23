4be3f9f9a2e391cd3b462671af30cc3a266d8749
RazikaBengana <razikabengana@gmail.com>
2024-06-23 19:09:25 +0200

    Add `SearchAvailabilityByDates` method to check room availability

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,14 @@
 package repository
 
-import "github.com/RazikaBengana/Go-BnB/internal/models"
+import (
+	"github.com/RazikaBengana/Go-BnB/internal/models"
+	"time"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
