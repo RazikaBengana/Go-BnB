1adbba157a15ae3f4dd24b3f3af620ef016fa400
RazikaBengana <razikabengana@gmail.com>
2024-06-23 02:01:15 +0200

    Implement reservation processing with date parsing and database insertion

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "github.com/RazikaBengana/Go-BnB/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+
+	InsertReservation(res models.Reservation) error
 }
