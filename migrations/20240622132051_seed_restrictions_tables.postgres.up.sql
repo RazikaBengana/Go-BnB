74a2a49fd78a64e994c75a4303c512ef5fb990b5
RazikaBengana <razikabengana@gmail.com>
2024-06-24 20:37:37 +0200

    Seed restrictions table with initial data for `Reservation` and `Owner Block`

---
+++ migrations/20240622132051_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+('Reservation','23-06-2024 00:00:00.000','23-06-2024 00:00:00.000'),
+('Owner Block','23-06-2024 00:00:00.000','23-06-2024 00:00:00.000');
