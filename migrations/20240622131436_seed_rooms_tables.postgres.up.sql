c25aa25ded25868c67e6102ebc15a08f59a40a13
RazikaBengana <razikabengana@gmail.com>
2024-06-24 20:31:29 +0200

    Seed rooms table with initial data for `General's Quarters` and `Major's Suite`

---
+++ migrations/20240622131436_seed_rooms_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+('General''s Quarters','23-06-2024 00:00:00.000','23-06-2024 00:00:00.000'),
+('Major''s Suite','23-06-2024 00:00:00.000','23-06-2024 00:00:00.000');
