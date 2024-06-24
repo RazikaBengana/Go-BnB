b46c6688a5c3d81633cad58b94cf35fc47a7f123
RazikaBengana <razikabengana@gmail.com>
2024-06-25 00:21:35 +0200

    Refactor tests to include scenarios for missing reservations and rooms

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"testing"
 )
 
@@ -19,17 +18,16 @@ var theTests = []struct {
 	name               string
 	url                string
 	method             string
-	params             []postData
 	expectedStatusCode int
 }{
+	{"home", "/", "GET", http.StatusOK},
+	{"about", "/about", "GET", http.StatusOK},
+	{"generals-quarters", "/generals-quarters", "GET", http.StatusOK},
+	{"majors-suite", "/majors-suite", "GET", http.StatusOK},
+	{"search-availability", "/search-availability", "GET", http.StatusOK},
+	{"contact", "/contact", "GET", http.StatusOK},
+
 	/*
-		{"home", "/", "GET", []postData{}, http.StatusOK},
-		{"about", "/about", "GET", []postData{}, http.StatusOK},
-		{"generals-quarters", "/generals-quarters", "GET", []postData{}, http.StatusOK},
-		{"majors-suite", "/majors-suite", "GET", []postData{}, http.StatusOK},
-		{"search-availability", "/search-availability", "GET", []postData{}, http.StatusOK},
-		{"contact", "/contact", "GET", []postData{}, http.StatusOK},
-		{"make-reservation", "/make-reservation", "GET", []postData{}, http.StatusOK},
 		{"post-search-availability", "/search-availability", "POST", []postData{
 			{key: "start", value: "2020-01-01"},
 			{key: "end", value: "2020-01-02"},
@@ -53,32 +51,14 @@ func TestHandlers(t *testing.T) {
 	defer ts.Close()
 
 	for _, e := range theTests {
-		if e.method == "GET" {
-			resp, err := ts.Client().Get(ts.URL + e.url)
+		resp, err := ts.Client().Get(ts.URL + e.url)
 
-			if err != nil {
-				t.Log(err)
-				t.Fatal(err)
-			}
-			if resp.StatusCode != e.expectedStatusCode {
-				t.Errorf("for %s, expected %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
-			}
-
-		} else {
-			values := url.Values{}
-
-			for _, x := range e.params {
-				values.Add(x.key, x.value)
-			}
-			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
-
-			if err != nil {
-				t.Log(err)
-				t.Fatal(err)
-			}
-			if resp.StatusCode != e.expectedStatusCode {
-				t.Errorf("for %s, expected %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
-			}
+		if err != nil {
+			t.Log(err)
+			t.Fatal(err)
+		}
+		if resp.StatusCode != e.expectedStatusCode {
+			t.Errorf("for %s, expected %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 		}
 	}
 }
@@ -105,6 +85,32 @@ func TestRepository_Reservation(t *testing.T) {
 
 	if rr.Code != http.StatusOK {
 		t.Errorf("Reservation handler returned wrong response code: got %d, want %d", rr.Code, http.StatusOK)
+	}
+
+	// Test case where reservation is not in session (reset everything)
+	req, _ = http.NewRequest("GET", "/make-reservation", nil)
+	ctx = getCtx(req)
+	req = req.WithContext(ctx)
+	rr = httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusTemporaryRedirect {
+		t.Errorf("Reservation handler returned wrong response code: got %d, want %d", rr.Code, http.StatusTemporaryRedirect)
+	}
+
+	// Test with non-existent room
+	req, _ = http.NewRequest("GET", "/make-reservation", nil)
+	ctx = getCtx(req)
+	req = req.WithContext(ctx)
+	rr = httptest.NewRecorder()
+	reservation.RoomID = 100
+	session.Put(ctx, "reservation", reservation)
+
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusTemporaryRedirect {
+		t.Errorf("Reservation handler returned wrong response code: got %d, want %d", rr.Code, http.StatusTemporaryRedirect)
 	}
 }
 
