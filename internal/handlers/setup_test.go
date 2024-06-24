31bf7630421921168deabd98e41c6c09a45b02fb
RazikaBengana <razikabengana@gmail.com>
2024-06-24 23:38:12 +0200

    Refactor `TestMain` setup for session management and template caching

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -15,6 +15,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"testing"
 	"time"
 )
 
@@ -23,7 +24,7 @@ var session *scs.SessionManager
 var pathToTemplates = "./../../templates"
 var functions = template.FuncMap{}
 
-func getRoutes() http.Handler {
+func TestMain(m *testing.M) {
 	// What I am going to put in the session
 	gob.Register(models.Reservation{})
 
@@ -58,6 +59,11 @@ func getRoutes() http.Handler {
 	repo := NewTestRepo(&app)
 	NewHandlers(repo)
 	render.NewRenderer(&app)
+
+	os.Exit(m.Run())
+}
+
+func getRoutes() http.Handler {
 
 	mux := chi.NewRouter()
 
