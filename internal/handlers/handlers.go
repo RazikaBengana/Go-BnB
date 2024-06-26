ce693a2688be59604153efc5607cc1bffe6fb78e
RazikaBengana <razikabengana@gmail.com>
2024-06-26 03:30:48 +0200

    Implement handler to mark reservation as processed

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -11,6 +11,7 @@ import (
 	"github.com/RazikaBengana/Go-BnB/internal/render"
 	"github.com/RazikaBengana/Go-BnB/internal/repository"
 	"github.com/RazikaBengana/Go-BnB/internal/repository/dbrepo"
+	"github.com/go-chi/chi"
 	"log"
 	"net/http"
 	"strconv"
@@ -612,4 +613,13 @@ func (m *Repository) AdminPostShowReservation(w http.ResponseWriter, r *http.Req
 // AdminReservationsCalendar displays the reservation calendar
 func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{})
+}
+
+// AdminProcessReservation marks a reservation as processed
+func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+	src := chi.URLParam(r, "src")
+	_ = m.DB.UpdateProcessedForReservation(id, 1)
+	m.App.Session.Put(r.Context(), "flash", "Reservation marked as processed")
+	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
 }
